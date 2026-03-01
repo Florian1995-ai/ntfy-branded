016bc5819f101140a047c5736b72a08894d74a2d
Florian Rolke <roelkeflorian@gmail.com>
2026-03-01 13:30:04 -0500

    Rename to Notification Center, double logo size, set base-url

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Start ntfy on port 8080 in background
-ntfy serve --listen-http :8080 &
+ntfy serve --listen-http :8080 --base-url https://push.florianrolke.com &
 
 # Start nginx in foreground
 nginx -g 'daemon off;'
